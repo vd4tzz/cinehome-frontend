b14800752104554157e9af4afc9d795fb3904cdb
d4tzz <d4tzz@temp.com>
2025-09-21 11:35:34 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+        <h1 className="text-red-900 text-5xl">Hello World</h1>
+    </div>
+
+  );
+}
+
+export default App
